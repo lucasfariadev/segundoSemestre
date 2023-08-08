1a60455aed5755e0f2e7654c73760d68a96bbbe1
Lucas Faria <74054553+lucasfariadev@users.noreply.github.com>
2023-08-07 21:21:57 -0300

    Add files via upload

---
+++ tecnicasDeProgramação/aula1/exemplos/src/exemplos/Exemplo1.java
@@ -0,0 +1,10 @@
+package exemplos;
+
+public class Exemplo1 {
+
+	public static void main(String[] args) {
+		int notaP1 = 10, notaP2 = 6, media = 0;
+		media = (notaP1 + notaP2) / 2;
+		System.out.println("A media do semestre é: " + media);
+	}
+}
---
+++ tecnicasDeProgramação/aula1/exemplos/src/exemplos/Exemplo3.java
@@ -0,0 +1,23 @@
+package exemplos;
+
+import java.util.Scanner;
+
+public class Exemplo3 {
+	
+	public static void main(String[] args) {
+		
+		float notaP1, notaP2, media; 
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		System.out.println("Informe a nota da P1: "); notaP1 = scanner.nextFloat();
+		System.out.println("Informe a nota da P2: "); notaP2 = scanner.nextFloat();
+		media = (notaP1+ notaP2)/2;
+		
+		System.out.println("A media final é: " + media);
+		
+		
+	}
+	
+
+}
